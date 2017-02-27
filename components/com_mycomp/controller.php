e12bb2730d1f5940fa1a31849a89379ed99c1d67
Van Uytven Stein <stein.van.uytven@pandora.be>
2017-02-27 13:58:08 +0100

    Disabele error reporting

--- components/com_mycomp/controller.php
+++ components/com_mycomp/controller.php
@@ -1,6 +1,7 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors',1);
+//error_reporting(E_ALL);
+//ini_set('display_errors',1);
+
 defined('_JEXEC') or die('Access denied');
 
 jimport('joomla.application.component.controller');
