9a75cb1298838c2309317c911e1604af725743bd
Van Uytven Stein <stein.van.uytven@pandora.be>
2017-02-27 14:34:50 +0100

    Backend, toolbar icon's

--- administrator/components/com_mycomp/controller.php
+++ administrator/components/com_mycomp/controller.php
@@ -9,31 +9,39 @@ jimport ('joomla.application.component.controller');
 class MyCompController extends JController{
 
 	function display(){
-		JToolbarHelper::Title('Default title!');
+		$doc=JFactory::getdocument();
+		$doc->addStyleSheet(JURI::root().'media/com_mycomp/css/mycomp.css');
+		JToolbarHelper::Title('Default title!','mycomp.png');
 		echo JText::_('COM_MYCOMP_WELCOME');
 	}
 	
 	function create(){
 		$doc=JFactory::getdocument();
+		$doc->addStyleSheet(JURI::root().'media/com_mycomp/css/mycomp.css');
 		$doc->addStyleSheet(JURI::root().'media/com_mycomp/css/backend_create_task.css');
 		$doc->addScript(JURI::root().'media/com_mycomp/js/backend_createtask.js');
-		JToolbarHelper::Title('Create task title!');
+		JToolbarHelper::Title('Create task title!','createtask.png');
 		echo '<div id="welcome">'. JText::_('COM_MYCOMP_TASK_CREATE') .'</div>';
-
 	}
 	
 	function delete(){
-		JToolbarHelper::Title('Delete task title!');
+		$doc=JFactory::getdocument();
+		$doc->addStyleSheet(JURI::root().'media/com_mycomp/css/mycomp.css');
+		JToolbarHelper::Title('Delete task title!','deletetask.png');
 		echo JText::_('COM_MYCOMP_TASK_DELETE');
 	}
 
 	function listtask(){
-		JToolbarHelper::Title('List task title!');
+		$doc=JFactory::getdocument();
+		$doc->addStyleSheet(JURI::root().'media/com_mycomp/css/mycomp.css');
+		JToolbarHelper::Title('List task title!','listtask.png');
 		echo JText::_('COM_MYCOMP_TASK_LISTTASK');
 	}	
 	
 	function help(){
-		JToolbarHelper::Title('Help task title!');
+		$doc=JFactory::getdocument();
+		$doc->addStyleSheet(JURI::root().'media/com_mycomp/css/mycomp.css');
+		JToolbarHelper::Title('Help task title!','helptask.png');
 		echo JText::_('COM_MYCOMP_TASK_HELP');
 		echo '<h1>This is help task <input type="text">';
 	}
