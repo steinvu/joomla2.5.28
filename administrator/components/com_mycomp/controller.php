35f4df5b89d45b9c14493f4eca6403334d3b9eda
Van Uytven Stein <stein.van.uytven@pandora.be>
2017-02-27 11:10:04 +0100

    Toolbar Title for tasks in backend

--- administrator/components/com_mycomp/controller.php
+++ administrator/components/com_mycomp/controller.php
@@ -6,22 +6,27 @@ jimport ('joomla.application.component.controller');
 class MyCompController extends JController{
 
 	function display(){
+		JToolbarHelper::Title('Default title!');
 		echo JText::_('COM_MYCOMP_WELCOME');
 	}
 	
 	function create(){
+		JToolbarHelper::Title('Create task title!');
 		echo JText::_('COM_MYCOMP_TASK_CREATE');
 	}
 	
 	function delete(){
+		JToolbarHelper::Title('Delete task title!');
 		echo JText::_('COM_MYCOMP_TASK_DELETE');
 	}
 
 	function listtask(){
+		JToolbarHelper::Title('List task title!');
 		echo JText::_('COM_MYCOMP_TASK_LISTTASK');
 	}	
 	
 	function help(){
+		JToolbarHelper::Title('Help task title!');
 		echo JText::_('COM_MYCOMP_TASK_HELP');
 		echo '<h1>This is help task <input type="text">';
 	}
